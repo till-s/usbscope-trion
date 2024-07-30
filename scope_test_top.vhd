8e515e554d86fa5174061c0ba0af70e2f95af317
till straumann <till.straumann@alumni.tu-berlin.de>
2024-07-30 23:56:31 +0200

    partial success
    - passes with ram clock 166.611MHz, ADC clk 123.747

--- scope_test_top.vhd
+++ scope_test_top.vhd
@@ -13,7 +13,6 @@ use     work.Usb2AppCfgPkg.all;
 use     work.CommandMuxPkg.all;
 use     work.BasicPkg.Slv8Array;
 use     work.SDRAMPkg.all;
-use     work.RMIIMacPkg.all;
 
 entity scope_test_top is
    generic (
@@ -25,10 +24,11 @@ entity scope_test_top is
       SDRAM_READ_DLY_G  : natural := 3;
       GIT_VERSION_G     : std_logic_vector(31 downto 0) := x"0000_0000";
       USE_SDRAM_BUF_G   : boolean := true;
-      ADC_FREQ_G        : real    := 120.0E6;
-      USE_SIN_GEN_G     : boolean := true;
-      NO_DECIMATORS_G   : boolean := true;
-      HAVE_SPI_CMD_G    : boolean := true
+      ADC_FREQ_G        : real    := 130.0E6;
+      USE_SIN_GEN_G     : boolean := false;
+      NO_DECIMATORS_G   : boolean := false;
+      HAVE_SPI_CMD_G    : boolean := true;
+      ADC_BITS_G        : natural := 10
    );
    port (
       sdram_clk         : in  std_logic;
@@ -59,38 +59,19 @@ entity scope_test_top is
       ulpiStp_IN        : in    std_logic                    := '0';
       ulpiStp_OUT       : out   std_logic                    := '0';
       ulpiStp_OE        : out   std_logic                    := '0';
-      LED               : out   std_logic_vector(7 downto 0) := (others => '1');
       ulpiPllLocked     : in    std_logic;
       sdramPllLocked    : in    std_logic;
 
       adcClk            : in    std_logic;
+      ADC_DDR_HI        : in    std_logic_vector(ADC_BITS_G downto 0);
+      ADC_DDR_LO        : in    std_logic_vector(ADC_BITS_G downto 0);
 
       spiSClk           : out   std_logic;
       spiMOSI           : out   std_logic;
       spiMISO           : in    std_logic;
       spiCSb_OUT        : out   std_logic;
       spiCSb_OE         : out   std_logic := '1';
-      spiCSb_IN         : in    std_logic;
-      
-      rmii_clk          : in    std_logic;
-      eth_rstb          : out   std_logic := '1';
-      eth_pwrdwn_irq_IN : in    std_logic;
-      eth_pwrdwn_irq_OUT: out   std_logic := '0';
-      eth_pwrdwn_irq_OE : out   std_logic := '0';
-
-      eth_mdc           : out   std_logic := '0';
-      eth_mdio_IN       : in    std_logic;
-      eth_mdio_OUT      : out   std_logic := '1';
-      eth_mdio_OE       : out   std_logic := '0';
-
-      eth_tx_en         : out   std_logic := '0';
-      eth_txd           : out   std_logic_vector(3 downto 0) := (others => '0');
-
-      eth_rx_dv         : in    std_logic;
-      eth_crs_dv        : in    std_logic;
-      eth_rx_col        : in    std_logic;
-      eth_rx_err        : in    std_logic;
-      eth_rxd           : in    std_logic_vector(3 downto 0)
+      spiCSb_IN         : in    std_logic
    );
 end entity scope_test_top;
 
@@ -108,37 +89,18 @@ architecture rtl of scope_test_top is
       return v;
    end function toSlv;
 
-   constant ADC_BITS_C         : natural := 10;
    -- must cover bulk max pkt size
    constant LD_FIFO_OUT_C      : natural :=  9;
    constant LD_FIFO_INP_C      : natural :=  9;
-   constant SDRAM_A_WIDTH_C    : natural := 12; -- row
+   constant SDRAM_A_WIDTH_C    : natural := 13; -- row
    constant SDRAM_B_WIDTH_C    : natural :=  2; -- bank
-   constant SDRAM_C_WIDTH_C    : natural :=  8; -- bank
+   constant SDRAM_C_WIDTH_C    : natural :=  9; -- bank
    constant FLAT_A_WIDTH_C     : natural := SDRAM_A_WIDTH_C + SDRAM_B_WIDTH_C + SDRAM_C_WIDTH_C;
    constant NSMPL_MAX_C        : natural := (2**FLAT_A_WIDTH_C * 4)/5;
 
    constant NUM_REGS_C         : natural := 20;
 
    constant MEM_DEPTH_C        : natural := ite( USE_SDRAM_BUF_G, 3354624, 2048 );
-
-   constant NCM_IF_ASSOC_IDX_C : integer := usb2NextIfcAssocDescriptor(
-      USB2_APP_DESCRIPTORS_C,
-      0,
-      USB2_IFC_CLASS_CDC_C,
-      USB2_IFC_SUBCLASS_CDC_NCM_C,
-      USB2_IFC_PROTOCOL_NONE_C
-   );
-
-   constant NCM_IFC_NUM_C      : natural := to_integer( unsigned( USB2_APP_DESCRIPTORS_C( NCM_IF_ASSOC_IDX_C + 2 ) ) );
-
-   constant EP0_AGENT_CFG_C    : Usb2CtlEpAgentConfigArray := (
-      0 => usb2CtlEpMkAgentConfig(
-         recipient => USB2_REQ_TYP_RECIPIENT_IFC_C,
-         index     => NCM_IFC_NUM_C,
-         reqType   => USB2_REQ_TYP_TYPE_VENDOR_C
-      )
-   );
 
    signal acmFifoOutDat        : Usb2ByteType;
    signal acmFifoOutEmpty      : std_logic;
@@ -188,9 +150,11 @@ architecture rtl of scope_test_top is
 
    signal syncDQIn             : std_logic_vector(15 downto 0);
 
-   signal adcDatA              : std_logic_vector(ADC_BITS_C downto 0) := (others => '0');
-   signal adcDatB              : std_logic_vector(ADC_BITS_C downto 0) := (others => '0');
-   signal adcCnt               : signed(ADC_BITS_C - 1 downto 0)       := (others => '0');
+   signal adcDatA              : std_logic_vector(ADC_BITS_G downto 0) := (others => '0');
+   signal adcDatB              : std_logic_vector(ADC_BITS_G downto 0) := (others => '0');
+   signal adcDatAReg           : std_logic_vector(ADC_BITS_G downto 0) := (others => '0');
+   signal adcDatBReg           : std_logic_vector(ADC_BITS_G downto 0) := (others => '0');
+   signal adcCnt               : signed(ADC_BITS_G - 1 downto 0)       := (others => '0');
 
    signal regRDat              : std_logic_vector(7 downto 0) := (others => '0');
    signal regWDat              : std_logic_vector(7 downto 0);
@@ -216,55 +180,7 @@ architecture rtl of scope_test_top is
    signal adcCini              : signed(17 downto 0);
    signal adcAini              : signed( 7 downto 0);
 
-   signal ncmFifoOutDat        : std_logic_vector(7 downto 0) := (others => '0');
-   signal ncmFifoOutLast       : std_logic := '0';
-   signal ncmFifoOutAbrt       : std_logic := '0';
-   signal ncmFifoOutEmpty      : std_logic := '0';
-   signal ncmFifoOutVld        : std_logic := '0';
-   signal ncmFifoOutHaveCrc    : std_logic := '0';
-   signal ncmFifoOutRen        : std_logic := '1';
-
-   signal ncmFifoInpDat        : std_logic_vector(7 downto 0) := (others => '0');
-   signal ncmFifoInpLast       : std_logic := '0';
-   signal ncmFifoInpAbrt       : std_logic := '0';
-   signal ncmFifoInpBusy       : std_logic := '0';
-   signal ncmFifoInpFull       : std_logic := '0';
-   signal ncmFifoInpWen        : std_logic := '0';
-
-   signal ncmCarrier           : std_logic := '1';
-   -- ECM, 6.2.4, Tbl-8: D(4): forward filtered (ordinary) mcst, 
-   --                    D(3): forward (ordinary) bcst
-   --                    D(2): forward (ordinary) ucst
-   --                    D(1): forward all multicast
-   --                    D(0): promiscuous mode
-   signal ncmPacketFilter      : std_logic_vector(4 downto 0) := "11111";
-   signal ncmSpeed             : unsigned(31 downto 0)        := to_unsigned( 100000000, 32 );
-   signal ncmSpeed10           : std_logic;
-
-   signal ethMacMcFilter       : EthMulticastFilterType := ETH_MULTICAST_FILTER_INIT_C;
-   signal ethMacAppendCrc      : std_logic := '1';
-   signal ethMacRxRdy          : std_logic := '0';
-   signal ethMacRxRst          : std_logic := '0';
-   signal ethMacRxAbrt         : std_logic := '0';
-   signal ethMacTxRst          : std_logic := '0';
-   signal ethMacSpeed10        : std_logic := '0';
-   signal ethMacLinkOk         : std_logic := '1';
-   signal ethMacColl           : std_logic := '0';
-   signal ethMacPromisc        : std_logic := '0';
-   signal ethMacAllmulti       : std_logic := '1';
-
-   signal usb2Ep0ReqParam      : Usb2CtlReqParamType;
-   signal usb2Ep0ObExt         : Usb2EndpPairObType;
-   signal usb2Ep0IbExt         : Usb2EndpPairIbArray( EP0_AGENT_CFG_C'range ) := (others => USB2_ENDP_PAIR_IB_INIT_C );
-   signal usb2Ep0CtlExt        : Usb2CtlExtArray( EP0_AGENT_CFG_C'range ):= (others => USB2_CTL_EXT_NAK_C);
-   signal mdioClk              : std_logic;
-   signal mdioDatOut           : std_logic;
-   signal mdioDatInp           : std_logic;
-   signal mdioDatHiZ           : std_logic;
-
 begin
-
-   assert false report "INTERFACE NCM " & integer'image(NCM_IFC_NUM_C);
 
    G_SYNC : if ( USE_SMPL_CLK_G ) generate
       G_SYNC_BIT : for i in sdram_DQ_IN'range generate
@@ -341,8 +257,8 @@ begin
       GIT_VERSION_G                => GIT_VERSION_G,
       FIFO_FREQ_G                  => 60.0E6,
       ADC_FREQ_G                   => ADC_FREQ_G,
-      ADC_BITS_G                   => ADC_BITS_C,
-      RAM_BITS_G                   => ADC_BITS_C,
+      ADC_BITS_G                   => ADC_BITS_G,
+      RAM_BITS_G                   => ADC_BITS_G,
       MEM_DEPTH_G                  => MEM_DEPTH_C,
       SDRAM_ADDR_WIDTH_G           => FLAT_A_WIDTH_C,
       USE_SDRAM_BUF_G              => USE_SDRAM_BUF_G,
@@ -390,8 +306,8 @@ begin
       adcRst                       => open,
 
       -- bit 0 is the DOR (overrange) bit
-      adcDataA                     => adcDatA,
-      adcDataB                     => adcDatB,
+      adcDataA                     => adcDatAReg,
+      adcDataB                     => adcDatBReg,
 
       extTrg                       => open, --: in  std_logic := '0';
 
@@ -409,9 +325,7 @@ begin
          LD_ACM_FIFO_DEPTH_INP_G   => LD_FIFO_INP_C,
          LD_ACM_FIFO_DEPTH_OUT_G   => LD_FIFO_OUT_C,
          CDC_ACM_ASYNC_G           => false,
-         CDC_NCM_ASYNC_G           => true,
          ULPI_EMU_MODE_G           => NONE,
-         CTL_EP0_AGENTS_CONFIG_G   => EP0_AGENT_CFG_C,
          MARK_DEBUG_ULPI_IO_G      => false,
          MARK_DEBUG_PKT_TX_G       => false,
          MARK_DEBUG_PKT_RX_G       => false,
@@ -429,11 +343,6 @@ begin
 
          usb2HiSpeedEn             => usb2HiSpeedEn,
 
-         usb2Ep0ReqParam           => usb2Ep0ReqParam,
-         usb2Ep0ObExt              => usb2Ep0ObExt,
-         usb2Ep0IbExt              => usb2Ep0IbExt,
-         usb2Ep0CtlExt             => usb2Ep0CtlExt,
-
          usb2DevStatus             => usb2DevStatus,
 
          acmFifoClk                => ulpiClk,
@@ -450,49 +359,8 @@ begin
 
          acmDTR                    => acmDTR,
          acmRate                   => acmRate,
-         acmParity                 => acmParity,
-
-         ncmFifoClk                => rmii_clk,
-         ncmFifoOutDat             => ncmFifoOutDat,
-         ncmFifoOutLast            => ncmFifoOutLast,
-         ncmFifoOutAbrt            => ncmFifoOutAbrt,
-         ncmFifoOutEmpty           => ncmFifoOutEmpty,
-         ncmFifoOutNeedCrc         => ncmFifoOutHaveCrc,
-         ncmFifoOutRen             => ncmFifoOutRen,
-
-         ncmFifoInpDat             => ncmFifoInpDat,
-         ncmFifoInpLast            => ncmFifoInpLast,
-         ncmFifoInpAbrt            => ncmFifoInpAbrt,
-         ncmFifoInpBusy            => ncmFifoInpBusy,
-         ncmFifoInpFull            => ncmFifoInpFull,
-         ncmFifoInpWen             => ncmFifoInpWen,
-
-         ncmCarrier                => ethMacLinkOk,
-         ncmPacketFilter           => ncmPacketFilter,
-         ncmSpeedInp               => ncmSpeed,
-         ncmSpeedOut               => ncmSpeed
+         acmParity                 => acmParity
       );
-
-   P_SPEED_SEL : process ( ncmSpeed10 ) is
-   begin
-      if ( ncmSpeed10 = '1' ) then
-         ncmSpeed <= to_unsigned(  10000000, ncmSpeed'length );
-      else
-         ncmSpeed <= to_unsigned( 100000000, ncmSpeed'length );
-      end if;
-   end process P_SPEED_SEL;
-
-   U_SYNC_SPEED : entity work.Usb2CCSync
-      generic map ( INIT_G => '0' )
-      port    map ( clk => ulpiClk,  d => ethMacSpeed10,      q => ncmSpeed10 );
-
-   U_SYNC_AMULT : entity work.Usb2CCSync
-      generic map ( INIT_G => '1' )
-      port    map ( clk => rmii_clk, d => ncmPacketFilter(1), q => ethMacAllmulti );
-
-   U_SYNC_PROMI : entity work.Usb2CCSync
-      generic map ( INIT_G => '0' )
-      port    map ( clk => rmii_clk, d => ncmPacketFilter(0), q => ethMacPromisc  );
 
    G_SDRAM_CTRL : if ( USE_SDRAM_BUF_G ) generate
 
@@ -523,7 +391,7 @@ begin
             sdramWEb                  => sdram_WEb,
             sdramCASb                 => sdram_CASb,
             sdramRASb                 => sdram_RASb,
-            sdramAddr                 => sdram_A(11 downto 0),
+            sdramAddr                 => sdram_A(SDRAM_A_WIDTH_C - 1 downto 0),
             sdramBank                 => sdram_BA,
             sdramDQInp                => syncDQIn,
             sdramDQOut                => sdram_DQ_OUT,
@@ -532,7 +400,7 @@ begin
 
    end generate G_SDRAM_CTRL;
 
-   sdram_A(12)   <= '0';
+   sdram_A(sdram_A'left downto SDRAM_A_WIDTH_C) <= ( others => '0' );
 
    sdram_DQ_OE   <= (others => sdramDQOE);
 
@@ -557,13 +425,15 @@ begin
          else
             adcCnt <= adcCnt + 1;
          end if;
+         adcDatAReg <= adcDatA;
+         adcDatBReg <= adcDatB;
       end if;
    end process P_SIM;
 
    G_ADC_CNT : if ( not USE_SIN_GEN_G ) generate
 
-   adcDatA <= std_logic_vector(     adcCnt ) & '0';
-   adcDatB <= std_logic_vector( 0 - adcCnt ) & '0';
+   adcDatA <= ADC_DDR_HI;
+   adcDatB <= ADC_DDR_LO;
 
    end generate G_ADC_CNT;
 
@@ -574,23 +444,8 @@ begin
       if ( rising_edge( ulpiClk ) ) then
          cnt := cnt + 1;
       end if;
-      LED(1) <= cnt(cnt'left);
    end process;
    
-   process ( rmii_clk ) is
-      variable cnt : integer range -1 to 49999998 := 49999998;
-      variable tgl : std_logic := '0';
-   begin
-      if ( rising_edge( rmii_clk ) ) then
-         if ( cnt < 0 ) then
-            cnt := 49999998;
-            tgl := not tgl;
-         else
-            cnt := cnt - 1;
-         end if;
-      end if;
-   end process;
-
    process ( regAddr, regIdx ) is
    begin
       regErr  <= '1';
@@ -615,6 +470,8 @@ begin
       end if;
    end process;
 
+   G_ADC_SIN : if ( USE_SIN_GEN_G ) generate
+
 --   U_SIN_COS : entity work.SinCosGen
 --      generic map (
 --         MULTIPLIER_G => "EFX"
@@ -629,133 +486,41 @@ begin
 --         sin      => adcSin
 --      );
 
-   U_COS : entity work.CosGen
-      port map (
-         clk      => adcClk,
-         load     => load,
-         coeff    => adcCoeff,
-         aini     => adcACini,
-         phasCos  => true,
-         cos      => adcCos
-      );
+      U_COS : entity work.CosGen
+         port map (
+            clk      => adcClk,
+            load     => load,
+            coeff    => adcCoeff,
+            aini     => adcACini,
+            phasCos  => true,
+            cos      => adcCos
+         );
 
-   U_SIN : entity work.CosGen
-      port map (
-         clk      => adcClk,
-         load     => load,
-         coeff    => adcCoeff,
-         aini     => adcASini,
-         phasCos  => false,
-         cos      => adcSin
-      );
+      U_SIN : entity work.CosGen
+         port map (
+            clk      => adcClk,
+            load     => load,
+            coeff    => adcCoeff,
+            aini     => adcASini,
+            phasCos  => false,
+            cos      => adcSin
+         );
 
-   P_REG_PARMS : process ( adcClk ) is
-   begin
-      if ( rising_edge( adcClk ) ) then
-         load     <= genLoad;
-         adcCoeff <= resize( signed( toSlv( regs( 0 to  2) ) ), adcCoeff'length );
-         adcCini  <= resize( signed( toSlv( regs( 3 to  5) ) ), adcCini'length  );
-         adcACini <= resize( signed( toSlv( regs( 6 to 11) ) ), adcACini'length );
-         adcASini <= resize( signed( toSlv( regs(12 to 17) ) ), adcASini'length );
-         adcAini  <= signed( regs(6) );
-      end if;
-   end process P_REG_PARMS;
+      P_REG_PARMS : process ( adcClk ) is
+      begin
+         if ( rising_edge( adcClk ) ) then
+            load     <= genLoad;
+            adcCoeff <= resize( signed( toSlv( regs( 0 to  2) ) ), adcCoeff'length );
+            adcCini  <= resize( signed( toSlv( regs( 3 to  5) ) ), adcCini'length  );
+            adcACini <= resize( signed( toSlv( regs( 6 to 11) ) ), adcACini'length );
+            adcASini <= resize( signed( toSlv( regs(12 to 17) ) ), adcASini'length );
+            adcAini  <= signed( regs(6) );
+         end if;
+      end process P_REG_PARMS;
 
-   G_ADC_SIN : if ( USE_SIN_GEN_G ) generate
-      adcDatA <= std_logic_vector( adcCos(adcCos'left downto adcCos'left - ADC_BITS_C + 1 ) ) & '0';
-      adcDatB <= std_logic_vector( adcSin(adcSin'left downto adcSin'left - ADC_BITS_C + 1 ) ) & '0';
+      adcDatA <= std_logic_vector( adcCos(adcCos'left downto adcCos'left - ADC_BITS_G + 1 ) ) & '0';
+      adcDatB <= std_logic_vector( adcSin(adcSin'left downto adcSin'left - ADC_BITS_G + 1 ) ) & '0';
    end generate G_ADC_SIN;
-
-   ncmFifoOutVld    <= not ncmFifoOutEmpty;
-   ethMacAppendCrc  <= not ncmFifoOutHaveCrc;
-
-   U_MAC_TX : entity work.RMIIMacTx
-      port map (
-         clk                          => rmii_clk,
-         rst                          => ethMacTxRst,
-         txDat                        => ncmFifoOutDat,
-         txVld                        => ncmFifoOutVld,
-         txLst                        => ncmFifoOutLast,
-         txRdy                        => ncmFifoOutRen,
-
-         rmiiDat                      => eth_txd(1 downto 0),
-         rmiiTxEn                     => eth_tx_en,
-
-         coll                         => ethMacColl,
-         speed10                      => ethMacSpeed10,
-         linkOK                       => ethMacLinkOk,
-         appendCRC                    => ethMacAppendCrc
-      );
-
-   ethMacRxRdy    <= (not ncmFifoInpBusy and not ncmFifoInpFull);
-   ethMacRxRst    <= ethMacRxAbrt or ethMacColl or not ethMacLinkOk;
-   ncmFifoInpAbrt <= ethMacRxRst;
-   ncmFifoOutAbrt <= ethMacColl;
-
-   U_MAC_RX : entity work.RMIIMacRx
-      port map (
-         clk                          => rmii_clk,
-         rst                          => ethMacRxRst,
-
-         rxDat                        => ncmFifoInpDat,
-         rxVld                        => ncmFifoInpWen,
-         rxLst                        => ncmFifoInpLast,
-         rxRdy                        => ethMacRxRdy,
-         rxAbt                        => ethMacRxAbrt,
-
-         rmiiDat                      => eth_rxd(1 downto 0),
-         rmiiDV                       => eth_rx_dv,
-
-         macAddr                      => x"31EFBEADDE02",
-         promisc                      => ethMacPromisc,
-         allmulti                     => ethMacAllmulti,
-         mcFilter                     => ethMacMcFilter,
-
-         -- misc
-         speed10                      => ethMacSpeed10,
-         stripCRC                     => '1'
-      );
-
-   eth_mdc        <= mdioClk;
-   mdioDatInp     <= eth_mdio_IN;
-   eth_mdio_OUT   <= mdioDatOut;
-   eth_mdio_OE    <= not mdioDatHiZ;
-
-   U_MDIO_CTL : entity work.Usb2Ep0MDIOCtl
-      generic map (
-         MDC_PRESCALER_G              => 3
-      )
-      port map (
-         usb2Clk                      => ulpiClk,
-         usb2Rst                      => usb2Rst,
-
- 
-         usb2CtlReqParam              => usb2Ep0ReqParam,
-         usb2CtlExt                   => usb2Ep0CtlExt(0),
-         usb2EpIb                     => usb2Ep0ObExt,
-         usb2EpOb                     => usb2Ep0IbExt(0),
-
-         mdioClk                      => mdioClk,
-         mdioDatOut                   => mdioDatOut,
-         mdioDatHiZ                   => mdioDatHiZ,
-         mdioDatInp                   => mdioDatInp,
-
-         speed10                      => ethMacSpeed10,
-         duplexFull                   => open, -- out std_logic := '0';
-         linkOk                       => ethMacLinkOk,
-         -- full contents; above bits are for convenience
-         statusRegPolled              => open
-      );
-
-   -- LEDs are active low
-   LED(7)        <= not ethMacPromisc;
-   LED(6)        <= not ethMacAllMulti;
-   LED(5)        <= not usb2DevStatus.suspended;
-   LED(4)        <= '1';
-   LED(3)        <= not ethMacSpeed10;
-   LED(2)        <= not ethMacLinkOk;
-
-   LED(0)        <= not sdramPllLocked;
 
    bbi           <= bbo;
 
